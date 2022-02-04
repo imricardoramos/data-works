1d2f5a81d6f6c8d5a083f38fabab56e989a83b6e
Ricardo Ramos <rcdo.ramos@gmail.com>
2022-02-04 01:23:02 -0300

    Adds CI

--- mix.exs
+++ mix.exs
@@ -10,7 +10,13 @@ defmodule DataWorks.MixProject do
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        data_works: [
+          include_executables_for: [:unix],
+          steps: [:assemble, :tar]
+        ]
+      ]
     ]
   end
 
